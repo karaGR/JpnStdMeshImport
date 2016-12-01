74e3083d7dba633f6a10cf7781e94d4a4d18f414
karaGR <hirohara501@gmail.com>
2016-12-02 08:26:08 +0900

    create plugin

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#===================================
+#標準地域メッシュコードインポートプラグイン
+#
+#===================================
+
+def classFactory(iface):
+    from .main import main
+    return main(iface)
