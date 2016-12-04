5171751560cd102de2ce33c321de6f60fbb0db64
karaGR <hirohara501@gmail.com>
2016-12-05 08:56:29 +0900

    GUIの作成をはじめたところ

--- main.py
+++ main.py
@@ -6,12 +6,25 @@ from PyQt4.QtGui import QLineEdit,QPushButton,QSpinBox
 from PyQt4.QtGui import QTableView,QStandardItemModel,QStandardItem
 
 import numpy as np
+from PySide.QtGui import QComboBox
+
+from collections import OrderedDict
 
 
 class main:
     
     def __init__(self,iface):
         self.iface = iface
+        self.kukaku_dict = QrderdDict() 
+        self.kukaku_dict[u"第1次地域区画"] = 4
+        self.kukaku_dict[u"第2次地域区画"] = 6
+        self.kukaku_dict[u"第3次地域区画"] = 8
+        self.kukaku_dict[u"5倍地域メッシュ"] = 7
+        self.kukaku_dict[u"2倍地域メッシュ"] = 9
+        self.kukaku_dict[u"2分の1地域メッシュ"] = 9
+        self.kukaku_dict[u"4分の1地域メッシュ"] = 10
+        self.kukaku_dict[u"8分の1地域メッシュ"] = 11
+        self.kukaku_dict[u"10分の１細分地域メッシュ"] = 10        
         
     def initGui(self):
         self.action = QAction(u"標準地域メッシュレイヤの追加",self.iface.mainWindow())
@@ -27,8 +40,9 @@ class main:
         
 class dlg_main(QDialog):
     
-    def __init__(self):
-       self.dlg_csv_error = dlg_csv_error(self) 
+    def __init__(self,pearent=pearent):
+        self.dlg_csv_error = dlg_csv_error(self) 
+        
     
     def initGui(self):
         self.VBL_main = QVBoxLayout()
@@ -63,7 +77,15 @@ class dlg_main(QDialog):
         self.ChB_recopt = QCheckBox()
         self.ChB_recopt.setText(u"最初のレコードはフィールド名を保持している")
         
-         
+        self.HBL_mesh = QHBoxLayout()
+        self.Lab_mesh_titile = QLabel(u"メッシュ定義")
+        
+        self.Lab_mesh_field = QLabel(u"メッシュコード列")
+        self.CoB_mesh_field = QComboBox()
+        
+        self.Lab_mesh_category = QLabel(u"地域メッシュ区画")
+        
+        
         
         
         
